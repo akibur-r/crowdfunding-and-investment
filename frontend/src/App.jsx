feab7ae8b0fe7f9f03f8e1e2830d9553e9eb64c3
Akram-Sa <sarkerakram98423@gmail.com>
2024-07-31 14:23:06 +0600

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,126 +1,123 @@
+import Signup from "./components/auth/signup";
+import Navbar from "./components/navbar/navbar";
 import "./layout.scss";
 import "./utility.scss";
-import Navbar from "./components/navbar/navbar";
-import Signup from './components/auth/signup'
 
+import Login from "./components/auth/login";
 import ViewBusiness from "./components/view/business/viewBusiness";
 import ViewCampaign from "./components/view/campaign/viewCampaign";
 import Homepage from "./routes/home/homepage";
-import Invest from "./components/transaction/invest";
-import AlertTest from "./components/alert/alertTest";
-import RequestSuccessAlert from "./components/alert/requestSuccessAlert";
-import RequestFailedAlert from "./components/alert/requestFailedAlert";
-import PublishedAlert from "./components/alert/publishedAlert";
-import PaymentSuccessful from "./components/alert/paymentSuccessful";
-import PaymentFailed from "./components/alert/paymentFailed";
-import DashboardMaster from './routes/dashboard/dashboardMaster'
-import Login from './components/auth/login'
 
 import * as React from "react";
-import { ReactDOM } from "react-dom/client";
-import {createBrowserRouter, RouterProvider, Link, Outlet, Router, Route, Routes} from 'react-router-dom'
-import Home from "./components/home/home";
-import Landingpage from "./components/home/landingpage";
-import Sidebar from "./routes/dashboard/sidebar";
+import {
+  createBrowserRouter,
+  Link,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+import Campaigns from "./routes/dashboard/campaigns";
 import Dashboard from "./routes/dashboard/dashboard";
 import Profile from "./routes/dashboard/profile";
-import Campaigns from "./routes/dashboard/campaigns";
-import Testing from "./testing";
-
+import Sidebar from "./routes/dashboard/sidebar";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: 
+    element: (
       <>
-        <Navbar/>
-        <Outlet/>
-      </>,
-    errorElement: <span>Not Found <br /> <Link to={"/"}>Go Home</Link></span>,
+        <Navbar />
+        <Outlet />
+      </>
+    ),
+    errorElement: (
+      <span>
+        Not Found <br /> <Link to={"/"}>Go Home</Link>
+      </span>
+    ),
     children: [
       {
         path: "/",
-        element: <Homepage/>
+        element: <Homepage />,
       },
       {
         path: "/start-a-campaign",
-        element: <h1>Start a Campaign</h1>
+        element: <h1>Start a Campaign</h1>,
       },
       {
         path: "/explore",
         children: [
           {
             path: "/explore",
-            element: <h1>Explore</h1>
+            element: <h1>Explore</h1>,
           },
           {
             path: "/explore/campaigns",
             children: [
               {
-                path: "/explore/campaigns"
+                path: "/explore/campaigns",
               },
               {
                 path: "/explore/campaigns/:campaignId",
-                element: <ViewCampaign/>
-              }
-            ]
+                element: <ViewCampaign />,
+              },
+            ],
           },
           {
             path: "/explore/businesses",
             children: [
               {
-                path: "/explore/businesses"
+                path: "/explore/businesses",
               },
               {
                 path: "/explore/businesses/:businessId",
-                element: <ViewBusiness/>
-              }
-            ]
-          }
-        ]
+                element: <ViewBusiness />,
+              },
+            ],
+          },
+        ],
       },
       {
         path: "/auth",
         children: [
           {
             path: "/auth/login",
-            element: <Login/>
+            element: <Login />,
           },
           {
             path: "/auth/signup",
-            element: <Signup/>
-          }
-        ]
+            element: <Signup />,
+          },
+        ],
       },
       {
         path: "/dashboard",
-        element: 
-        <div className='dashboardMaster'>
-          <Sidebar/>
-          <Outlet/>
-        </div>,
+        element: (
+          <div className="dashboardMaster">
+            <Sidebar />
+            <Outlet />
+          </div>
+        ),
         children: [
           {
             path: "/dashboard/overview",
-            element: <Dashboard/>
+            element: <Dashboard />,
           },
           {
             path: "/dashboard/profile",
-            element: <Profile/>
+            element: <Profile />,
           },
           {
             path: "/dashboard/campaigns",
-            element: <Campaigns/>
+            element: <Campaigns />,
           },
           {
             path: "/dashboard/businesses",
-            element: <h1>Businesses</h1>
-          }
-        ]
-      }
-    ]
-  }
-  
+            element: <h1>Businesses</h1>,
+          },
+        ],
+      },
+    ],
+  },
 ]);
 
 function App() {
@@ -133,11 +130,9 @@ function App() {
         <PaymentFailed/>
       </div> */}
 
-      {/* <RouterProvider router={router} /> */}
-      <Testing/>
-
+      <RouterProvider router={router} />
     </div>
-  )
+  );
 }
 
 export default App;
