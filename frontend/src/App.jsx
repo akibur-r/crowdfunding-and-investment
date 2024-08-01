ad47b92153b9ebb1515a2b5401a55d184d393aa1
Akram-Sa <sarkerakram98423@gmail.com>
2024-08-01 08:48:49 +0600

    The router comment on the function app has been fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,123 +1,114 @@
-import Signup from "./components/auth/signup";
+import Signup from './components/auth/signup';
 import Navbar from "./components/navbar/navbar";
 import "./layout.scss";
 import "./utility.scss";
 
-import Login from "./components/auth/login";
+import Login from './components/auth/login';
 import ViewBusiness from "./components/view/business/viewBusiness";
 import ViewCampaign from "./components/view/campaign/viewCampaign";
 import Homepage from "./routes/home/homepage";
 
 import * as React from "react";
-import {
-  createBrowserRouter,
-  Link,
-  Outlet,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, Link, Outlet, RouterProvider } from 'react-router-dom';
 import Campaigns from "./routes/dashboard/campaigns";
 import Dashboard from "./routes/dashboard/dashboard";
 import Profile from "./routes/dashboard/profile";
 import Sidebar from "./routes/dashboard/sidebar";
 
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: (
+    element: 
       <>
-        <Navbar />
-        <Outlet />
-      </>
-    ),
-    errorElement: (
-      <span>
-        Not Found <br /> <Link to={"/"}>Go Home</Link>
-      </span>
-    ),
+        <Navbar/>
+        <Outlet/>
+      </>,
+    errorElement: <span>Not Found <br /> <Link to={"/"}>Go Home</Link></span>,
     children: [
       {
         path: "/",
-        element: <Homepage />,
+        element: <Homepage/>
       },
       {
         path: "/start-a-campaign",
-        element: <h1>Start a Campaign</h1>,
+        element: <h1>Start a Campaign</h1>
       },
       {
         path: "/explore",
         children: [
           {
             path: "/explore",
-            element: <h1>Explore</h1>,
+            element: <h1>Explore</h1>
           },
           {
             path: "/explore/campaigns",
             children: [
               {
-                path: "/explore/campaigns",
+                path: "/explore/campaigns"
               },
               {
                 path: "/explore/campaigns/:campaignId",
-                element: <ViewCampaign />,
-              },
-            ],
+                element: <ViewCampaign/>
+              }
+            ]
           },
           {
             path: "/explore/businesses",
             children: [
               {
-                path: "/explore/businesses",
+                path: "/explore/businesses"
               },
               {
                 path: "/explore/businesses/:businessId",
-                element: <ViewBusiness />,
-              },
-            ],
-          },
-        ],
+                element: <ViewBusiness/>
+              }
+            ]
+          }
+        ]
       },
       {
         path: "/auth",
         children: [
           {
             path: "/auth/login",
-            element: <Login />,
+            element: <Login/>
           },
           {
             path: "/auth/signup",
-            element: <Signup />,
-          },
-        ],
+            element: <Signup/>
+          }
+        ]
       },
       {
         path: "/dashboard",
-        element: (
-          <div className="dashboardMaster">
-            <Sidebar />
-            <Outlet />
-          </div>
-        ),
+        element: 
+        <div className='dashboardMaster'>
+          <Sidebar/>
+          <Outlet/>
+        </div>,
         children: [
           {
             path: "/dashboard/overview",
-            element: <Dashboard />,
+            element: <Dashboard/>
           },
           {
             path: "/dashboard/profile",
-            element: <Profile />,
+            element: <Profile/>
           },
           {
             path: "/dashboard/campaigns",
-            element: <Campaigns />,
+            element: <Campaigns/>
           },
           {
             path: "/dashboard/businesses",
-            element: <h1>Businesses</h1>,
-          },
-        ],
-      },
-    ],
-  },
+            element: <h1>Businesses</h1>
+          }
+        ]
+      }
+    ]
+  }
+  
 ]);
 
 function App() {
@@ -131,8 +122,9 @@ function App() {
       </div> */}
 
       <RouterProvider router={router} />
+
     </div>
-  );
+  )
 }
 
 export default App;
